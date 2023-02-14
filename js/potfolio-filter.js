b72789e63cfefca44a6515a701027bcbb43c5329
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-02-14 11:16:47 +0200

    додає анімацію та фільтр кнопок портфоліо

--- js/potfolio-filter.js
+++ js/potfolio-filter.js
@@ -1,5 +1,27 @@
-const filterBox = document.querySelectorAll('.section-portfolio-list')
 
-document.querySelector('nav').addEventListener('click', (event)=> {
+function app () {
+    const buttons = document.querySelectorAll('.button-portfolio')
+    const cards = document.querySelectorAll('.section-portfolio-list')
 
-});
+    function filter (category, items) {
+        items.forEach((item) => {
+            const isItemFiltered = !item.classList.contains(category)
+            const isShowAll = category.toLowerCase() === 'all'
+            if (isItemFiltered && !isShowAll) {
+                item.classList.add('hide')
+            } else {
+                item.classList.remove('hide')
+            }
+        })
+    }
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const currentCategory = button.dataset.filter
+            filter(currentCategory, cards)
+        })
+
+    })
+}
+
+app ()
